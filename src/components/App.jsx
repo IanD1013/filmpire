6c077111db98da72f4d976716f11439a3cd4a204
IanD1013 <ian.tung1013@gmail.com>
2024-11-16 19:23:22 +1300

    Set up basic app structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+
+import { Movies, Actors, MovieInfo, NavBar, Profile } from './';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <CssBaseline />
+      <NavBar />
+      <main>
+        <Routes>
+          <Route exact path="/" element={<Movies />} />
+          <Route exact path="/movie/:id" element={<MovieInfo />} />
+          <Route exact path="/actors/:id" element={<Actors />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  );
 };
 
 export default App;
