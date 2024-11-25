e41f9eae429f223a092b3da4547f2ab6a244df0a
IanD1013 <ian.tung1013@gmail.com>
2024-11-25 14:27:35 +1300

    Adjust number of movies to display on lg devices

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -10,6 +10,9 @@ const Movies = () => {
   const { genreIdOrCategoryName, searchQuery } = useSelector((state) => state.currentGenreOrCategory);
   const { data, error, isFetching } = useGetMoviesQuery({ genreIdOrCategoryName, page, searchQuery });
 
+  const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
+  const numberOfMovies = lg ? 16 : 18;
+
   if (isFetching) {
     return (
       <Box display="flex" justifyContent="center">
@@ -34,7 +37,7 @@ const Movies = () => {
 
   return (
     <div>
-      <MovieList movies={data} />
+      <MovieList movies={data} numberOfMovies={numberOfMovies} />
       <Pagination currentPage={page} setPage={setPage} totalPages={data.total_pages} />
     </div>
   );
